6c98ebe2971f259d20ad8a64a765463e3c2f888e
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-03-29 23:20:39 -0400

    app.modules configured

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,34 +1,41 @@
 // Modules
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 // Components
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './partials/header/header.component';
-import { FooterComponent } from './partials/footer/footer.component';
-import { BasePageComponent } from './partials/base-page/base-page.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { ContactListComponent } from './contacts/contact-list/contact-list.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
-import { ContactDeleteComponent } from './contacts/contact-delete/contact-delete.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./partials/header/header.component";
+import { FooterComponent } from "./partials/footer/footer.component";
+import { BasePageComponent } from "./partials/base-page/base-page.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { ProjectsComponent } from "./pages/projects/projects.component";
+import { ServicesComponent } from "./pages/services/services.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+import { ContactListComponent } from "./contacts/contact-list/contact-list.component";
+import { RegisterComponent } from "./pages/register/register.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { ContactDetailsComponent } from "./contacts/contact-details/contact-details.component";
+import { ContactDeleteComponent } from "./contacts/contact-delete/contact-delete.component";
 
 // Services
-import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
-import { AuthService } from './services/auth.service';
-import { JwtModule, JwtHelperService, JwtInterceptor } from '@auth0/angular-jwt';
+import {
+  FlashMessagesModule,
+  FlashMessagesService
+} from "angular2-flash-messages";
+import { AuthService } from "./services/auth.service";
+import {
+  JwtModule,
+  JwtHelperService,
+  JwtInterceptor
+} from "@auth0/angular-jwt";
 
 export function jwtTokenGetter() {
-  return localStorage.getItem('id_token');
+  return localStorage.getItem("id_token");
 }
 
 @NgModule({
@@ -39,7 +46,7 @@ export function jwtTokenGetter() {
     BasePageComponent,
     HomeComponent,
     AboutComponent,
-    ProductsComponent,
+    ProjectsComponent,
     ServicesComponent,
     ContactComponent,
     PageNotFoundComponent,
@@ -64,4 +71,4 @@ export function jwtTokenGetter() {
   providers: [FlashMessagesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
