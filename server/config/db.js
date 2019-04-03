84fd277e2d479e5873843d055f2e71eefec6e754
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-04-02 22:13:21 -0400

    mLab string corrected

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = {
-  URI: "mongodb://localhost/test",
+  //URI: "mongodb://localhost/test",
   //URI: "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
-  //URI: "mongodb://Bishnu:Bikalpa22@ds056288.mlab.com:56288/comp308-w2019-assignment-2",
+  URI:
+    "mongodb://Bishnu:Bikalpa22@ds056288.mlab.com:56288/comp308-w2019-assignment-2",
   secret: "SomeSecret"
 };
