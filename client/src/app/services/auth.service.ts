7f5ae1ba40d64a3f8d301c7f66a03fb56f955cb8
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-04-05 00:19:59 -0400

    tested after code fix in servces page

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -9,7 +9,6 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { JwtHelperService } from "@auth0/angular-jwt";
-
 import { User } from "../models/user";
 @Injectable({
   providedIn: "root"
--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  URI: "mongodb://localhost/test",
+  // URI: "mongodb://localhost/test",
   URI:
     "mongodb://Bishnu:Bikalpa22@ds056288.mlab.com:56288/comp308-w2019-assignment-2",
   secret: "SomeSecret"
